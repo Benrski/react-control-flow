983a678c3d0e5aca7f033027bcd403a8e062e89c
Benrski <benjaminarski@gmail.com>
2022-09-11 19:05:04 +0200

    feat: basic lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'ReactControlFlow',
+      fileName: 'react-control-flow',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
