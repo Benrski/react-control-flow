cb46026dd4a32f8e7c016e229267565fd5cb025d
Benrski <benjaminarski@gmail.com>
2022-10-01 14:59:31 +0200

    feat: Switch component

---
+++ src/lib/Switch.tsx
@@ -0,0 +1,44 @@
+import type { ReactElement, ReactNode } from 'react';
+import { Children, isValidElement } from 'react';
+
+export interface MatchProps<T> {
+  when: T | undefined | null | false;
+  children: ReactNode | ((item: NonNullable<T>) => ReactNode);
+}
+
+export function Match<T>({
+  when,
+  children,
+}: MatchProps<T>): ReactElement | null {
+  if (when) {
+    return <>{typeof children === 'function' ? children(when) : children}</>;
+  }
+
+  return null;
+}
+
+export interface SwitchProps {
+  fallback?: ReactNode;
+  children: ReactNode;
+}
+
+export function Switch({
+  children,
+  fallback,
+}: SwitchProps): ReactElement | null {
+  const match = Children.toArray(children).find(
+    (child) => isValidElement(child) && child.type === Match && child.props.when
+  );
+
+  if (match) {
+    return <>{match}</>;
+  }
+
+  if (fallback) {
+    return <>{fallback}</>;
+  }
+
+  return null;
+}
+
+Switch.Match = Match;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,5 @@ export { For } from './For';
 export type { ForProps } from './For';
 export { Show } from './Show';
 export type { ShowProps } from './Show';
+export { Match, Switch } from './Switch';
+export type { MatchProps, SwitchProps } from './Switch';
